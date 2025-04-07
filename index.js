b6eedfde68658647c3433412a31a0fe3d29ba165
shashank-poola <shashankpoola123@gmail.com>
2025-04-07 11:32:08 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json());
+
+app.post('/todos', (res,req) => {
+
+})
+
+app.get('/todos', (res,req) => {
+
+})
+
+app.put('/completed', (res,req) => {
+
+})
