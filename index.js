582c36c52f55b145a54a6cf6d8c9738431a71c58
shashank-poola <shashankpoola123@gmail.com>
2025-04-07 11:48:55 +0530

    improved the express

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require("express")
+const express = require("express");
+const { updateTodo } = require("./types");
+const { createServerParamsForRoute } = require("next/dist/server/request/params");
 const app = express()
 
 app.use(express.json());
 
 app.post('/todos', (res,req) => {
-
+    const createPayLoad = req.body;
+    const parsedPayLoad = createTodo.safeParse(createPayload);
+    if (!parsedPayLoad.success) {
+        res.status(411).json({
+            msg: "You sent the wrond inputs"
+        })
+        return;
+    }
 })
 
 app.get('/todos', (res,req) => {
@@ -13,4 +22,12 @@ app.get('/todos', (res,req) => {
 
 app.put('/completed', (res,req) => {
 
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayloadPayload);
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        })
+        return;
+    }
 })
